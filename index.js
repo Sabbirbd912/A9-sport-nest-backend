3b9faf54b3b83d90242c392a900de7bee8db7662
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-06-27 00:37:47 +0600

    delete of facilities

--- index.js
+++ index.js
@@ -118,6 +118,15 @@ async function run() {
         },
       };
 
+      //delete api
+      app.delete("/deletefacility/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        // const query = { _id: id };
+        const result = await facilitiesCollection.deleteOne(query);
+        res.send(result);
+      });
+
       const result = await facilitiesCollection.updateOne(query, update); //updating command
       res.send(result); //sending response
     });
